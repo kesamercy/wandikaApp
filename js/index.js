87e42449873923a8a9bb900a3f30957b575a143f
Mercy Nekesa <kesamercy@gmail.com>
2019-10-14 10:40:55 -0400

    new version of wandika app

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("a").click(function() {
+
+    $("#loginForm").load("./loginPage.php");
+});
+
+$("a").click(function() {
+
+    $("#signUpForm").load("./signUpPage.php");
+});
---
+++ loginPage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Wandika</title>
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <form action="">
+            <div class="loginBox">
+                Login here
+                <input type="email" name="email" value="email" onFocus="field_focus(this, 'email');"
+                    onblur="field_blur(this, 'email');" class="email" />
+                <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');"
+                    onblur="field_blur(this, 'email');" class="email" />
+                <input type="button" value="submit" class="submitButton">
+            </div>
+
+        </form>
+
+    </body>
+
+</html>
