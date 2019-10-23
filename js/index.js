be1b60a48d79961b46044d0b90e7a3819c0970bd
Mercy Nekesa <kesamercy@gmail.com>
2019-10-23 13:33:29 -0400

    added elements for the aboutPage. styling still needs more work

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 $("a").click(function() {
+    $("#aboutPage").load("./about.html");
+});
 
+$("a").click(function() {
     $("#loginForm").load("./loginPage.php");
 });
 
 $("a").click(function() {
-
     $("#signUpForm").load("./signUpPage.php");
 });
