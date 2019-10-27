1a18b27dc1980e361cf53809df03b613cd93c33a
Mercy Nekesa <kesamercy@gmail.com>
2019-10-27 09:55:03 -0400

    added api for create account and login

--- loginPage.php
+++ loginPage.php
@@ -10,17 +10,26 @@
         <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
     <body>
         <form action="">
-            <div class="loginBox">
-                Login here
-                <input type="email" name="email" value="email" onFocus="field_focus(this, 'email');"
-                    onblur="field_blur(this, 'email');" class="email" />
-                <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');"
-                    onblur="field_blur(this, 'email');" class="email" />
-                <input type="button" value="submit" class="submitButton">
+            <div class="loginPage_loginBox">
+                <br> <br> <img class="loginPage_logo" src="images\logo-wandika-word.png" alt="logo"><br>
+                <i class="fa fa-envelope icon loginPage_icons"></i>
+                <input class="loginPage_inputFields" type="email" name="email" value="Email">
+                <br>
+                <i class="fa fa-key icon loginPage_icons"></i>
+                <input class="loginPage_inputFields" type="password" name="password" value="Password">
+                <input type="button" value="Login" class="submitButton">
+                <br>
+                <div class="loginPage_forgotPsswdLink">
+                    <a href=""> Forgot Password?</a>
+
+                </div>
+
             </div>
 
         </form>
